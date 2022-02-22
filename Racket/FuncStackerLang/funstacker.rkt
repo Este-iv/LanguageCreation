4cbf956c65e99e16867aaaacd211dde7a59bcfd3
Este <eserna@iastate.edu>
2022-02-22 13:01:19 -0600

    update and addition.
    Finalized the Stackerlang.
    Addition of Funclang - Func lang is a functional version of Stackerlang. It is being worked on and rewritten.

---
+++ Racket/FuncStackerLang/funstacker.rkt
@@ -0,0 +1,36 @@
+#lang br/quicklang
+
+
+(define (read-syntax path port)
+  (define src-lines (port->lines port))
+  (define src-datums (format-datums ' ~a src-lines))
+  (define module-datum `(module funstacker-mod "funstacker.rkt"
+                          (handle-args ,@src-datums)))
+  (datum->syntax #f module-datum))
+(provide read-syntax)
+
+
+(define-macro (funstacker-module-begin HANDLE-EXPR ...)
+  #'(#%module-begin
+     (display (first HANDLE-ARGS-EXPR))))
+(provide (rename-out [funstacker-module-begin #%module-begin]))
+
+(define stack empty)
+
+(define (pop-stack!)
+  (define arg (first stack))
+  (set! stack (rest stack))
+  arg)
+
+(define (push-stack! arg)
+  (set! stack (cons arg stack)))
+
+(define (handle [arg #f])
+  (cond
+    [(number? arg) (push-stack! arg)]
+    [(or (equal? + arg) (equal? * arg))
+     (define op-result (arg (pop-stack!) (pop-stack!)))
+     (push-stack! op-result)]))
+(provide handle)
+
+(provide + *)
