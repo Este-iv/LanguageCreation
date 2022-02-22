0f285035840ada0bf3a230ec2b90b39bd8107010
Este <eserna@iastate.edu>
2022-02-22 11:59:21 -0600

    updates.
    -handle function added for addition and multiplication.
    - I should implement subtraction and division.

--- Racket/StackerLang/stacker.rkt
+++ Racket/StackerLang/stacker.rkt
@@ -12,7 +12,8 @@
 
 (define-macro (stacker-module-begin HANDLE-EXPR ...)
   #'(#%module-begin
-     'HANDLE-EXPR ...))
+     HANDLE-EXPR ...
+     (display (first stack))))
 (provide (rename-out [stacker-module-begin #%module-begin]))
 
 (define stack empty)
@@ -24,3 +25,13 @@
 
 (define (push-stack! arg)
   (set! stack (cons arg stack)))
+
+(define (handle [arg #f])
+  (cond
+    [(number? arg) (push-stack! arg)]
+    [(or (equal? + arg) (equal? * arg))
+     (define op-result (arg (pop-stack!) (pop-stack!)))
+     (push-stack! op-result)]))
+(provide handle)
+
+(provide + *)
