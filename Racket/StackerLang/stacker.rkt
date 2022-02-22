316bbb33dec98786c5af96e69077d2cd9bbe3718
Este <eserna@iastate.edu>
2022-02-22 11:30:45 -0600

    new additions.
    - Binding expansion added.
    - A stack has been implemented.
    - pop and push have been implemented.

--- Racket/StackerLang/stacker.rkt
+++ Racket/StackerLang/stacker.rkt
@@ -1,5 +1,6 @@
 #lang br/quicklang
 
+
 (define (read-syntax path port)
   (define src-lines (port->lines port))
   (define src-datums (format-datums '(handle ~a) src-lines))
@@ -7,3 +8,19 @@
                           ,@src-datums))
   (datum->syntax #f module-datum))
 (provide read-syntax)
+
+
+(define-macro (stacker-module-begin HANDLE-EXPR ...)
+  #'(#%module-begin
+     'HANDLE-EXPR ...))
+(provide (rename-out [stacker-module-begin #%module-begin]))
+
+(define stack empty)
+
+(define (pop-stack!)
+  (define arg (first stack))
+  (set! stack (rest stack))
+  arg)
+
+(define (push-stack! arg)
+  (set! stack (cons arg stack)))
